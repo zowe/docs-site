fd2dbe8fc793e0c65afb1eeefdd0ee61bfb76732
Adam-Battenburg <Adam.Battenburg@ibm.com>
2018-08-22 09:43:41 -0400

    Ejected from default theme and added header.

---
+++ docs/.vuepress/theme/Page.vue
@@ -0,0 +1,121 @@
+<template>
+  <div class="page">
+    <page-header/>
+    <Content :custom="false"/>
+    <div class="page-nav" v-if="prev || next">
+      <p class="inner">
+        <span v-if="prev" class="prev">
+          ← <router-link v-if="prev" class="prev" :to="prev.path">
+            {{ prev.title || prev.path }}
+          </router-link>
+        </span>
+        <span v-if="next" class="next">
+          <router-link v-if="next" :to="next.path">
+            {{ next.title || next.path }}
+          </router-link> →
+        </span>
+      </p>
+    </div>
+    <slot name="bottom"/>
+  </div>
+</template>
+
+<script>
+import { resolvePage } from './util'
+import PageHeader from './PageHeader.vue'
+
+export default {
+  components: {
+    PageHeader
+  },
+  props: ['sidebarItems'],
+  computed: {
+    lastUpdated() {
+      if (this.$page.lastUpdated) {
+        return new Date(this.$page.lastUpdated).toLocaleString(this.$lang)
+      }
+    },
+    lastUpdatedText() {
+      if (typeof this.$themeLocaleConfig.lastUpdated === 'string') {
+        return this.$themeLocaleConfig.lastUpdated
+      }
+      if (typeof this.$site.themeConfig.lastUpdated === 'string') {
+        return this.$site.themeConfig.lastUpdated
+      }
+      return 'Last Updated'
+    },
+    prev() {
+      const prev = this.$page.frontmatter.prev
+      if (prev === false) {
+        return
+      } else if (prev) {
+        return resolvePage(this.$site.pages, prev, this.$route.path)
+      } else {
+        return resolvePrev(this.$page, this.sidebarItems)
+      }
+    },
+    next() {
+      const next = this.$page.frontmatter.next
+      if (next === false) {
+        return
+      } else if (next) {
+        return resolvePage(this.$site.pages, next, this.$route.path)
+      } else {
+        return resolveNext(this.$page, this.sidebarItems)
+      }
+    }
+  }
+}
+
+function resolvePrev(page, items) {
+  return find(page, items, -1)
+}
+
+function resolveNext(page, items) {
+  return find(page, items, 1)
+}
+
+function find(page, items, offset) {
+  const res = []
+  items.forEach(item => {
+    if (item.type === 'group') {
+      res.push(...(item.children || []))
+    } else {
+      res.push(item)
+    }
+  })
+  for (let i = 0; i < res.length; i++) {
+    const cur = res[i]
+    if (cur.type === 'page' && cur.path === page.path) {
+      return res[i + offset]
+    }
+  }
+}
+</script>
+
+<style lang="stylus">
+@import './styles/config.styl';
+@require './styles/wrapper.styl';
+
+.page {
+  padding-bottom: 2rem;
+}
+
+.page-nav {
+  @extend $wrapper;
+  padding-top: 1rem;
+  padding-bottom: 0;
+
+  .inner {
+    min-height: 2rem;
+    margin-top: 0;
+    border-top: 1px solid $borderColor;
+    padding-top: 1rem;
+    overflow: auto; // clear float
+  }
+
+  .next {
+    float: right;
+  }
+}
+</style>
