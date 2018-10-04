e79ed33ba705a163e88d770d7b1e93240182e7d8
BrandonJenkins14 <jenbr05@ca.com>
2018-10-04 14:04:19 -0400

    file name - use incorrect file name so that links don't break
    Signed-off-by: BrandonJenkins14 <jenbr05@ca.com>

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -41,7 +41,7 @@ module.exports = {
         {
           title: 'Onboard Spring Boot REST API services using Zowe API Mediation Layer',
           collapsable: true,
-          children: ['api-mediation-onboard-a-spring-boot-rest-api-service']
+          children: ['api-mediation-onboard-a-sprint-boot-rest-api-service']
         },
         {
           title: 'Onboard an existing Java REST API service without Spring Boot with Zowe API Mediation Layer',
