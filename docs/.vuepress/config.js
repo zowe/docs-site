958ece167349d78fa6cd2869e0971e6ca9f069ee
Ashley Li <nannanli@cn.ibm.com>
2018-11-14 21:48:33 +0800

    Merge branch 'master' into docs_reorg

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -40,81 +40,28 @@ module.exports = {
       }
     },
     nav: [
-      { text: 'Developer Tutorials', link: '/guides/intro' },
-      { text: 'Samples', link: '/samples/intro' },
-      { text: 'User Guide', link: '/user-guide/aboutthisdoc' },
+      { text: 'Getting started', link: '/getting-started/overview' },
+      { text: 'User Guide', link: '/user-guide/installandconfig' },
+      { text: 'Extending', link: '/extend/zlux-example-server' },
+      { text: 'Troubleshooting', link: '/troubleshoot/troubleshootinstall' },
+      { text: 'Contributing', link: '/contribute/CONTRIBUTING' },
       { tags: ['versions'] }, // versions dropdown placeholder, it should be converted
       { text: 'Zowe.org', link: 'https://zowe.org' }
     ],
     sidebar: {
-      '/guides/': [
-        'intro',
+      '/getting-started/': [
         {
-          title: 'Getting Started with zLUX',
+          title: 'Zowe overview',
           collapsable: true,
-          children: ['zlux-example-server', 'zlux-workshop-user-browser', 'zlux-tutorials']
-        },
-        {
-          title: 'Providing Liberty APIs',
-          collapsable: true,
-          children: ['libertyAPI', 'ReactJSUI']
-        },
-        {
-          title: 'Onboard REST APIs to the API Mediation Layer',
-          collapsable: true,
-          children: [
-            'api-mediation-onboard-overview',
-            'api-mediation-onboard-a-sprint-boot-rest-api-service',
-            'api-mediation-onboard-an-existing-java-rest-api-service-without-spring-boot-with-zowe-api-mediation-layer',
-            'api-mediation-onboard-an-existing-java-jersey-rest-api-service',
-            'api-mediation-onboard-an-existing-rest-api-service-without-code-changes']
-        },
-        {
-          title: 'Developing for Zowe CLI',
-          collapsable: true,
-          children: ['cli-devTutorials', 'cli-setting-up', 'cli-installing-sample-plugin', 'cli-extending-a-plugin', 'cli-developing-a-plugin',
-          'cli-implement-profiles']
-        }
-      ],
-      '/samples/': [
-        'intro',
-        {
-          title: 'Starter App Samples',
-          collapsable: true,
-          children: ['starter-intro', 'zlux-workshop-starter-app.md']
-        },
-        {
-          title: 'API Extension Samples',
-          collapsable: true,
-          children: ['api-intro', 'liberty-api-sample']
-        },
-        {
-          title: 'zLUX Samples',
-          collapsable: true,
-          children: [
-            'ui-intro',
-            'iframe-sample',
-            'react-sample',
-            'angular-sample'
-          ]
-        }
-      ],
-      '/user-guide/': [
-        {
-          title: 'About this documentation',
-          collapsable: true,
-          children: ['aboutthisdoc']
+          children: ['overview']
         },
         {
           title: 'Release notes',
           collapsable: true,
           children: ['summaryofchanges']
         },
-        {
-          title: 'Zowe overview',
-          collapsable: true,
-          children: ['overview']
-        },
+      ],
+      '/user-guide/': [
         {
           title: 'Installing Zowe',
           collapsable: true,
@@ -146,7 +93,25 @@ module.exports = {
           ]
         },
         {
-          title: 'Extending the Zowe Application Framework',
+          title: 'Zowe extensions and plug-ins',
+          collapsable: true,
+          children: [
+            'cli-extending',
+            'cli-installplugins',
+            'cli-cicsplugin',
+            'cli-db2plugin',
+            'cli-vscodeplugin'
+          ]
+        }
+        ],
+      '/extend/': [
+        {
+          title: 'The Zowe Extender Environment',
+          collapsable: true,
+          children: ['zlux-example-server']
+        },
+        {
+          title: 'Developing for Zowe Application Framework (zLUX)',
           collapsable: true,
           children: [
             'mvd-extendingzlux',
@@ -158,20 +123,62 @@ module.exports = {
             'mvd-uribroker',
             'mvd-apptoappcommunication',
             'mvd-errorreportingui',
-            'mvd-logutility'
+            'mvd-logutility',
+            'zlux-example-server',
+            'zlux-workshop-user-browser',
+            'zlux-tutorials',
+            'starter-intro',
+            'zlux-workshop-starter-app.md',
+            'ui-intro',
+            'iframe-sample',
+            'react-sample',
+            'angular-sample'
           ]
         },
         {
-          title: 'Extending Zowe CLI',
+          title: 'Developing for API Mediation Layer',
           collapsable: true,
           children: [
-            'cli-extending',
-            'cli-installplugins',
-            'cli-cicsplugin',
-            'cli-db2plugin',
-            'cli-vscodeplugin'
-          ]
+            'api-mediation-onboard-overview',
+            'api-mediation-onboard-a-sprint-boot-rest-api-service',
+            'api-mediation-onboard-an-existing-java-rest-api-service-without-spring-boot-with-zowe-api-mediation-layer',
+            'api-mediation-onboard-an-existing-java-jersey-rest-api-service',
+            'api-mediation-onboard-an-existing-rest-api-service-without-code-changes']
+        },
+        {
+          title: 'Developing for Zowe CLI',
+          collapsable: true,
+          children: [
+            'cli-devTutorials',
+            'cli-setting-up',
+            'cli-installing-sample-plugin',
+            'cli-extending-a-plugin',
+            'cli-developing-a-plugin',
+            'cli-implement-profiles']
+        },
+        {
+          title: 'Developing JEE components',
+          collapsable: true,
+          children: [
+            'libertyAPI',
+            'ReactJSUI',
+            'api-intro',
+            'liberty-api-sample']
         }
+      ],
+	'/troubleshoot/': [
+        {
+          title: 'FAQ',
+          collapsable: true,
+          children: ['troubleshootinstall']
+        },
+      ],
+	'/contribute/': [
+        {
+          title: 'Contributing to documentation',
+          collapsable: true,
+          children: ['CONTRIBUTING']
+        },
       ],
       '/': ['about']
     }
--- docs/guides/intro.md
+++
@@ -1,3 +0,0 @@
-# Developer Tutorials 
-
-Follow the tutorials in this section to learn how to develop or contribute to various Zowe components.
