a2f7c0572ec9f2abe8f467abaf82c7aa0b3cccf0
Jack (T.) Jia <jack-tiefeng.jia@ibm.com>
2019-05-13 09:25:20 -0400

    hook to right page loading events
    Signed-off-by: Jack (T.) Jia <jack-tiefeng.jia@ibm.com>

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -323,7 +323,14 @@ module.exports = {
     ['script', {
       src: 'https://cdnjs.cloudflare.com/ajax/libs/clipboard.js/2.0.0/clipboard.min.js'
     }],
-    ['script', { }, `window.addEventListener('load', function() {
+    ['script', { }, `window.ensureCopyToClipboard = function() {
+      var existing = document.querySelectorAll('pre[class*="language-"] > button.btn-copy-to-clipboard');
+      if (existing.length > 0) {
+        console.log('copy-to-clipboard already initialized.')
+        return false;
+      }
+      console.log('copy-to-clipboard not initialized yet, continuing ...')
+
       const copyIcon =
         '<svg width="12" height="12" viewBox="340 364 14 15" xmlns="http://www.w3.org/2000/svg"><path fill="currentColor" d="M342 375.974h4v.998h-4v-.998zm5-5.987h-5v.998h5v-.998zm2 2.994v-1.995l-3 2.993 3 2.994v-1.996h5v-1.995h-5zm-4.5-.997H342v.998h2.5v-.997zm-2.5 2.993h2.5v-.998H342v.998zm9 .998h1v1.996c-.016.28-.11.514-.297.702-.187.187-.422.28-.703.296h-10c-.547 0-1-.452-1-.998v-10.976c0-.546.453-.998 1-.998h3c0-1.107.89-1.996 2-1.996 1.11 0 2 .89 2 1.996h3c.547 0 1 .452 1 .998v4.99h-1v-2.995h-10v8.98h10v-1.996zm-9-7.983h8c0-.544-.453-.996-1-.996h-1c-.547 0-1-.453-1-.998 0-.546-.453-.998-1-.998-.547 0-1 .452-1 .998 0 .545-.453.998-1 .998h-1c-.547 0-1 .452-1 .997z" fill-rule="evenodd"/></svg>';
       const clearTooltip = function(e) {
@@ -346,7 +353,7 @@ module.exports = {
           }
           return actionMsg;
         };
-      var snippets = document.querySelectorAll('pre.language-text > code');
+      var snippets = document.querySelectorAll('pre[class*="language-"] > code');
       snippets.forEach(function(snippet) {
         snippet.parentNode.firstChild.insertAdjacentHTML('beforebegin', '<button class="btn-copy-to-clipboard" title="Copy to clipboard" data-clipboard-snippet>'+copyIcon+'</button>')
       });
@@ -355,7 +362,7 @@ module.exports = {
           btns[i].addEventListener('mouseleave', clearTooltip);
           btns[i].addEventListener('blur', clearTooltip);
       }
-      var clipboardSnippets = new ClipboardJS('[data-clipboard-snippet]',{
+      var clipboardSnippets = new ClipboardJS('[data-clipboard-snippet]', {
           target: function(trigger) {
               return trigger.parentNode.querySelector('code');
           }
@@ -367,7 +374,7 @@ module.exports = {
       clipboardSnippets.on('error', function(e) {
           showTooltip(e.trigger, fallbackMessage(e.action));
       });
-    });`],
+    };`],
     //embed hotjar
     ['script', { }, `(function(h,o,t,j,a,r){
         h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
