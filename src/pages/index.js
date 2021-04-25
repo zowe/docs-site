2bd21b9e1623431885e43bf9e0417fdc182c766e
Arijit Kundu <kunduarijit55@gmail.com>
2021-04-26 03:10:00 +0530

    feat: revamp the UI of main home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,40 +1,71 @@
-import React from 'react';
-import clsx from 'clsx';
-import Layout from '@theme/Layout';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import styles from './index.module.css';
-import HomepageFeatures from '../components/HomepageFeatures';
+import React from "react";
+import clsx from "clsx";
+import Layout from "@theme/Layout";
+import Link from "@docusaurus/Link";
+import useBaseUrl from "@docusaurus/useBaseUrl";
+// import Translate, {translate} from '@docusaurus/Translate';
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import { DocSearch } from "@docsearch/react";
+import styles from "./styles.module.css";
 
-function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
-          </Link>
-        </div>
-      </div>
-    </header>
-  );
-}
+//Components
+import ExploreContent from "../components/ExploreContent/ExploreContent";
+import DiscoverYourPath from "../components/DiscoverYourPath/DiscoverYourPath";
+import FeaturedTopics from "../components/FeaturedTopics/FeaturedTopics";
+import DownloadableFiles from "../components/DownloadableFiles/DownloadableFiles";
 
-export default function Home() {
-  const {siteConfig} = useDocusaurusContext();
+//Icons
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faDownload } from "@fortawesome/free-solid-svg-icons";
+
+function Home() {
+  const context = useDocusaurusContext();
+  const { siteConfig = {} } = context;
   return (
     <Layout
       title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
-      <HomepageHeader />
+      description="Description will go into a meta tag in <head />"
+    >
+      <header className={clsx("hero", styles.heroBanner)}>
+        <div className="container">
+          <h1 className="hero__title">{siteConfig.title}</h1>
+          <p className="hero__subtitle">{siteConfig.tagline}</p>
+          {/* <div className="display-flex justify-content--center margin-vert--md">
+            <div className={clsx('margin-right--md', styles.buttons)}>
+              <Link
+                className={clsx(
+                  'button button--outline button--lg',
+                  styles.getStarted,
+                )}
+                to={useBaseUrl('docs/')}>
+                <FontAwesomeIcon icon={ faDownload } className="margin-right--md"/>
+                Download Zowe
+              </Link>
+            </div>
+            <div className={styles.buttons}>
+              <Link
+                className={clsx(
+                  'button button--outline button--lg',
+                  styles.getStarted,
+                )}
+                to={useBaseUrl('docs/')}>
+                Try Zowe
+              </Link>
+            </div>
+          </div> */}
+          <div className="searchDiv">
+            <DocSearch apiKey="aa" indexName="aa" />
+          </div>
+        </div>
+      </header>
+      <ExploreContent />
+      <DiscoverYourPath />
+      <FeaturedTopics />
       <main>
-        <HomepageFeatures />
+        <DownloadableFiles />
       </main>
     </Layout>
   );
 }
+
+export default Home;
