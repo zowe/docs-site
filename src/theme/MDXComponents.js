edafa24938a63114894f0fecf2e428b66d538e0d
Arijit Kundu <kunduarijit55@gmail.com>
2021-05-18 23:05:24 +0530

    feat: added zoom-in effect for DoCItem Images

--- src/theme/MDXComponents.js
+++ src/theme/MDXComponents.js
@@ -1,9 +1,11 @@
 import InitialMDXComponents from "@theme-init/MDXComponents";
 import Badge from "./Badge";
+import img from "./ZoomedImage";
 
 const MDXComponents = {
   ...InitialMDXComponents,
   Badge: Badge,
+  img: img,
 };
 
 export default MDXComponents;
