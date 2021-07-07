3527f1f8a3471430c26f9c8c0ea209163b11db43
nannanli <nannanli@cn.ibm.com>
2021-07-07 11:48:51 +0800

    remove all files on master
    Signed-off-by: nannanli <nannanli@cn.ibm.com>

--- docs/.vuepress/plugins/copy-to-clipboard/index.js
+++
@@ -1,7 +0,0 @@
-const { path } = require('@vuepress/shared-utils')
-
-module.exports = (options) => ({
-  name: 'copy-to-clipboard',
-
-  clientRootMixin: path.resolve(__dirname, 'clientRootMixin.js')
-})
