c30addf20cf0c325fc39b5bf7168fb76c98fe7a6
Joe-Winchester <winchest@uk.ibm.com>
2022-02-07 09:13:28 +0000

    Fixed sidebars.js error
    Signed-off-by: Joe-Winchester <winchest@uk.ibm.com>

--- sidebars.js
+++ sidebars.js
@@ -136,7 +136,6 @@ module.exports = {
         }, 
         "user-guide/verify-zowe-runtime-install",
         "user-guide/start-stop-zowe-zos",
-        "user-guide/stop-zowe-zos",
         "user-guide/upgrade-zos-system",
         "user-guide/zowe-zos-uninstall",       
       ],
