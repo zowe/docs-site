c067dea5dccd029b40e73304140dc002c53346ce
Timothy Johnson <timothy.johnson@broadcom.com>
2022-08-22 10:43:04 -0400

    Make scripts smarter to determine output dir
    Signed-off-by: Timothy Johnson <timothy.johnson@broadcom.com>

--- scripts/updateWebHelp.sh
+++ scripts/updateWebHelp.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
-# Usage: bash scripts/updateWebHelp.sh <zoweVersion>
+# Usage: bash scripts/updateWebHelp.sh <zoweVersion> [outputDir]
 
 set -e
 ZOWE_VERSION=$1
+OUTPUT_DIR=$2
 
-# Navigate to static directory
-cd "$(dirname "$0")"
-cd ../static/$(echo v$ZOWE_VERSION | sed 's/.$/x/')
+# Navigate to output directory
+if [ -z "$OUTPUT_DIR" ]; then
+  baseDir=$(dirname "$0")/..
+  OUTPUT_DIR=$baseDir/static/$(echo v$ZOWE_VERSION | sed 's/.$/x/')
+  if ! grep -q $(basename "$OUTPUT_DIR") "$baseDir/versions.json"; then
+    OUTPUT_DIR=$(dirname "$OUTPUT_DIR")/stable
+  fi
+fi
+cd $OUTPUT_DIR
 
 # Clean up old files
 rm -rf web_help
