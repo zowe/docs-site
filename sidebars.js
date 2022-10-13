3a4d502ae17121a4d15e14570a651a72ac0fd71a
anaxceron <ana.ceron@broadcom.com>
2022-10-13 14:29:56 -0400

    added supported environs, reorganized FTP ext doc
    Signed-off-by: anaxceron <ana.ceron@broadcom.com>

--- sidebars.js
+++ sidebars.js
@@ -291,10 +291,16 @@ module.exports = {
             "user-guide/cli-ftpplugin",
             "user-guide/cli-imsplugin",
             "user-guide/cli-mqplugin",
-            "user-guide/ze-install",
           ],
         },
-        "user-guide/ze-usage",
+        {
+          type: "category",
+          label: "Using Zowe Explorer",
+          items: [
+                "user-guide/ze-usage",
+                "user-guide/ze-use-cases",
+         ],
+            },
          {
           type: "category",
           label: "Zowe Explorer extensions",
@@ -312,7 +318,16 @@ module.exports = {
                 "user-guide/ze-provide-feedback-contribute",
                               ],
             },
-            "user-guide/ze-ftp"
+            {
+              type: "category",
+              label: "Zowe Explorer FTP Extension",
+              items: [
+                "user-guide/ze-ftp-install-ze-ftp-ext",
+                "user-guide/ze-ftp-using-ze-ftp-ext",
+                "user-guide/ze-ftp-supported-functions",
+                "user-guide/ze-ftp-provide-feedback-contribute",
+                              ],
+            },
           ],
         },
         "user-guide/sdks-using",
