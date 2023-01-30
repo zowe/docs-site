8063b3365697d856614f8774cde6f82741a82710
1000TurquoisePogs <sgrady@rocketsoftware.com>
2023-01-30 08:17:21 -0500

    Update sidebars.js
    Add the component registries page to the sidebar
    
    Signed-off-by: 1000TurquoisePogs <sgrady@rocketsoftware.com>

--- sidebars.js
+++ sidebars.js
@@ -487,6 +487,7 @@ module.exports = {
         "extend/lifecycling-with-zwesvstc",
         "extend/k8s-extend",
         "extend/k8s-conformance",
+        "extend/component-registries"
       ],
     },
     {
