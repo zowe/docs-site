a79514312ce4d5e840bcf9e510868b1412abf0e7
Andrew W. Harn <andrew.harn@broadcom.com>
2023-10-12 10:44:49 -0400

    Revert "Revert "Merge branch 'docs-staging' of https://github.com/zowe/docs-site into docs-staging""
    This reverts commit a1e5df656a1b925872c7ab6fd5bf6f0bdeb20af0.
    Signed-off-by: Andrew W. Harn <andrew.harn@broadcom.com>

--- sidebars.js
+++ sidebars.js
@@ -27,6 +27,7 @@ module.exports = {
         "getting-started/zowe-high-availability"
       ],
     },
+    "appendix/zowe-glossary", 
     {
       type: "category",
       label: "Zowe FAQ",
@@ -859,7 +860,7 @@ module.exports = {
       ]
     },
     "appendix/zowe-yaml-configuration",
-    "appendix/server-component-manifest",
+    "appendix/server-component-manifest",    
     "appendix/bill-of-materials",
     {
       type: "link",
