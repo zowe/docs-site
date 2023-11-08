3aced0a6b48d5aa1d5fcf6a0bda6406d9cd1274a
1000TurquoisePogs <sgrady@rocketsoftware.com>
2023-11-08 11:25:22 -0500

    Update sidebars.js
    Signed-off-by: 1000TurquoisePogs <sgrady@rocketsoftware.com>

--- sidebars.js
+++ sidebars.js
@@ -678,6 +678,7 @@ module.exports = {
           items: [
             "troubleshoot/app-framework/app-mustgather",
             "troubleshoot/app-framework/app-issue",
+            "troubleshoot/app-framework/appfw-tracing",
             "troubleshoot/app-framework/app-return-codes",
             "troubleshoot/app-framework/appserver-error-codes",
             "troubleshoot/app-framework/zss-error-codes",
