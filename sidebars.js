6cfebef45c203bd9b94967dc5d17f1997a1f7dff
Jakub Balhar <jakub.balhar@broadcom.com>
2023-11-09 13:35:24 +0100

    Add new page to the sidebar.
    Signed-off-by: Jakub Balhar <jakub.balhar@broadcom.com>

--- sidebars.js
+++ sidebars.js
@@ -218,6 +218,7 @@ module.exports = {
                 "user-guide/api-mediation/discovery-service-configuration",
                 "user-guide/api-mediation/api-mediation-internal-configuration",
                 "extend/extend-apiml/api-mediation-passtickets",
+                "user-guide/api-mediation/api-mediation-multi-tenancy"
               ],
             },
           ],
