c265d7cad61ed228becafb1be7f3dac57bc91e0f
anaxceron <ana.ceron@broadcom.com>
2024-01-31 15:33:37 -0500

    reorg of checklist
    Signed-off-by: anaxceron <ana.ceron@broadcom.com>

--- sidebars.js
+++ sidebars.js
@@ -293,8 +293,8 @@ module.exports = {
           label: "Zowe CLI",
           link: {type:"doc", id:"user-guide/user-roadmap-zowe-cli"},
           items: [
-            "user-guide/systemrequirements-cli",
             "user-guide/cli-install-cli-checklist",
+            "user-guide/systemrequirements-cli",
           {
           type: "category",
           label: "Installing Zowe CLI",
@@ -307,7 +307,8 @@ module.exports = {
           },
             "user-guide/install-cli-via-proxy",
             "user-guide/cli-updatingcli",
-            "user-guide/cli-uninstall",   
+            "user-guide/cli-uninstall",  
+            "user-guide/cli-install-verify-your-installation", 
             {
               type: "category",
               label: "Advanced Zowe CLI configuration",
@@ -500,7 +501,6 @@ module.exports = {
               link: {type:"doc", id:"user-guide/cli-using-using-team-profiles"},
               items: [
                 "user-guide/cli-using-initializing-team-configuration",
-                "user-guide/cli-using-test-zosmf-connection",
                 "user-guide/cli-using-team-configuration-application-developers",
                 "user-guide/cli-using-team-configuration-team-leaders",
                 "user-guide/cli-using-sharing-team-config-files",
