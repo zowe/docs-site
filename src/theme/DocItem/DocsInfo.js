cfc4083d65cea3c31e6c2a7220108d1d2b405230
Timothy Johnson <timothy.johnson@broadcom.com>
2024-02-05 08:26:52 -0500

    Clean up unused imports
    Signed-off-by: Timothy Johnson <timothy.johnson@broadcom.com>

--- src/theme/DocItem/DocsInfo.js
+++ src/theme/DocItem/DocsInfo.js
@@ -1,16 +1,10 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
-import { useActiveVersion } from "@docusaurus/plugin-content-docs/client";
-import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import useBaseUrl from "@docusaurus/useBaseUrl";
 import styles from "./styles.module.css";
 import ShareButton from "./ShareButton";
 
 function DocsInfo({ docsPluginId, ...props }) {
-  const { siteConfig } = useDocusaurusContext();
-  const currentVersion = useActiveVersion(docsPluginId).label.split(" ")[0];
-  const latestVersion = siteConfig.customFields.latestVersion;
-
   const location = useLocation();
   const openDocIssueURL =
     "https://github.com/zowe/docs-site/issues/new?assignees=&labels=&template=---doc-error-report.md&title=Issue with docs.zowe.org" +
--- src/theme/NotFound/index.js
+++ src/theme/NotFound/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import {translate} from '@docusaurus/Translate';
 import {PageMetadata} from '@docusaurus/theme-common';
 import Layout from '@theme/Layout';
-import NotFoundContent from '@theme-original/NotFound/Content';
+import NotFoundContent from '@theme/NotFound/Content';
+
 export default function Index() {
   const title = translate({
     id: 'theme.NotFound.title',
