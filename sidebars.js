ec88c3d8ea8b2a1f19f3695990362a691dd0f290
Andrew Jandacek <andrew.jandacek@broadcom.com>
2024-02-23 10:14:46 +0100

    add missing comma
    Signed-off-by: Andrew Jandacek <andrew.jandacek@broadcom.com>

--- sidebars.js
+++ sidebars.js
@@ -107,7 +107,7 @@ module.exports = {
             "user-guide/systemrequirements-zos",
             "user-guide/install-nodejs-zos",
             "user-guide/address-security-requirements",
-            "user-guide/address-authentication-requirements"
+            "user-guide/address-authentication-requirements",
             "user-guide/configure-uss",
             "user-guide/address-storage-requirements",
             "user-guide/address-network-requirements",
