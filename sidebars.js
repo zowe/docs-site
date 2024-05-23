5d163f118dc799f3cc33251e889e4a3e6c98bb35
Andrew Jandacek <andrew.jandacek@broadcom.com>
2024-05-23 11:52:37 +0200

    Revert "global AT-TLS configuration (#3668)"
    This reverts commit 3b3213e104ddfcacd9b574ae366b6258551983ce.

--- sidebars.js
+++ sidebars.js
@@ -200,7 +200,6 @@ module.exports = {
                 "user-guide/generate-certificates",
                 "user-guide/use-certificates",
                 "user-guide/certificates-setup",
-                "user-guide/at-tls-configuration",
               ],
             },
             {
