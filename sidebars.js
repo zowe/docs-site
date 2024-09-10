c3783f4e1b644044a3f4eb555dd722a9bd801680
Andrew Jandacek <andrew.jandacek@broadcom.com>
2024-09-10 15:07:10 +0200

    fix sidebar refs
    Signed-off-by: Andrew Jandacek <andrew.jandacek@broadcom.com>

--- sidebars.js
+++ sidebars.js
@@ -127,7 +127,7 @@ module.exports = {
             "user-guide/address-browser-requirements",
           ]
         },
-      "user-guide/install-zowe-server-install-wizard-1-0-1",
+      "user-guide/install-zowe-server-install-wizard",
         {
           type: "category",
           label: "Installing Zowe via SMP/E",
