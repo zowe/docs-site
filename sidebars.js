779bcb14ccd39d12195e7b2d373c7c4ba1bec45b
Pablo Hernán Carle <pablo.carle@broadcom.com>
2024-10-25 15:09:37 +0200

    at-tls to master
    Signed-off-by: Pablo Hernán Carle <pablo.carle@broadcom.com>

--- sidebars.js
+++ sidebars.js
@@ -325,7 +325,6 @@ module.exports = {
                   ]
                 },
                 "user-guide/api-mediation/configuration-customizing-the-api-catalog-ui",
-                "user-guide/api-mediation/configuration-at-tls",
                 "user-guide/api-mediation/configuration-logging",
                 "user-guide/api-mediation/wto-message-on-startup",
               ]
