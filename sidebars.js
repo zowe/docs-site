ae1221791c0ae94f82b335ce6ac0d8295ae4aca0
Andrew Jandacek <andrew.jandacek@broadcom.com>
2025-10-22 14:11:01 +0200

    add 2_18_3 to sidebars.js
    Signed-off-by: Andrew Jandacek <andrew.jandacek@broadcom.com>

--- sidebars.js
+++ sidebars.js
@@ -21,7 +21,8 @@ module.exports = {
       label: "Zowe V2 release notes",
       className: "ToCheadercolor",
       collapsed: true,
-      items: [      
+      items: [   
+        "whats-new/release-notes/v2_18_3",   
         "whats-new/release-notes/v2_18_2",  
         "whats-new/release-notes/v2_18_1",
         "whats-new/release-notes/v2_18_0",
