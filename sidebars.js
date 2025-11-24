a8b3fe2f214fea63a21480ffb3be85bc197d2a2d
anaxceron <ana.ceron@broadcom.com>
2025-11-24 13:05:22 -0500

    bumping doc site release
    Signed-off-by: anaxceron <ana.ceron@broadcom.com>

--- sidebars.js
+++ sidebars.js
@@ -9,6 +9,7 @@ module.exports = {
       link: { type: "doc", id: "whats-new/release-notes/release-notes-overview" },
       collapsed: false,
       items: [
+        "whats-new/release-notes/v3_4_0",
         "whats-new/release-notes/v3_3_1",
         "whats-new/release-notes/v3_3_0",
         "whats-new/release-notes/v3_2_0",
@@ -951,7 +952,7 @@ module.exports = {
     {
       type: "link",
       label: "Third Party Software Requirements",
-      href: "https://github.com/zowe/docs-site/tree/master/tpsr/tpsr-v3.3.x.md",
+      href: "https://github.com/zowe/docs-site/tree/master/tpsr/tpsr-v3.4.x.md",
     },
   ],
 }
