b8a941f4c84ca7aba5e322b09ab896d3524e35f1
1000TurquoisePogs <sgrady@rocketsoftware.com>
2026-02-27 14:59:10 -0500

    Restore dead core loggers page
    Signed-off-by: 1000TurquoisePogs <sgrady@rocketsoftware.com>

--- sidebars.js
+++ sidebars.js
@@ -847,6 +847,7 @@ module.exports = {
             "troubleshoot/app-framework/app-mustgather",
             "troubleshoot/app-framework/app-issue",
             "troubleshoot/app-framework/appfw-tracing",
+            "troubleshoot/app-framework/core-loggers",
             "troubleshoot/app-framework/app-return-codes",
             "troubleshoot/app-framework/appserver-error-codes",
             "troubleshoot/app-framework/zss-error-codes",
